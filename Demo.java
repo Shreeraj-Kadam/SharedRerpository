d23f8c39481c39e1acffaf5dd521a9c07e64366b
Shreeraj-Kadam <143249284+Shreeraj-Kadam@users.noreply.github.com>
2023-08-26 15:16:24 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ class Demo{
 public static void main(String[] args){
 System.out.println("This is git practice");
 System.out.println("This is 2nd Git Practice");
+  System.out.println("This is 3rd Git Practice");
 }
 }
